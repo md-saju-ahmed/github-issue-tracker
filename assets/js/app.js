a99dbc97699f79c8e9b543b4dabd48801b662f97
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-03-08 15:51:09 +0600

    added toggle filter buttons js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,35 @@ const showSpinner = (status) => {
         document.getElementById("spinner").classList.add("hidden");
     }
 };
+
+// Toggle filter buttons
+const toggleFilter = (btnId) => {
+
+    // Inactive button
+    document.querySelectorAll("#filter-btns button").forEach(btn => {
+        btn.classList.remove("bg-[#4A00FF]", "hover:bg-[#3C00D6]", "text-white");
+        btn.classList.add("text-[#64748b]", "border-[#e4e4e7]");
+    });
+
+    // Active button
+    const activeBtn = document.getElementById(btnId);
+    activeBtn.classList.remove("text-[#64748b]", "border-[#e4e4e7]");
+    activeBtn.classList.add("bg-[#4A00FF]", "hover:bg-[#3C00D6]", "text-white");
+
+    // Show spinner
+    showSpinner(true);
+
+    // Issue filtering
+    setTimeout(() => {
+        if (btnId === "all-filter-btn") {
+            displayIssues(allIssuesData);
+        } else if (btnId === "open-filter-btn") {
+            const openIssues = allIssuesData.filter(issue => issue.status === "open");
+            displayIssues(openIssues);
+        } else if (btnId === "closed-filter-btn") {
+            const closedIssues = allIssuesData.filter(issue => issue.status === "closed");
+            displayIssues(closedIssues);
+        }
+        showSpinner(false); // Hide spinner
+    }, 100);
+};
