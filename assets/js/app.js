737b5c73cf0f25b73af8816207acb60b43aeca15
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-03-08 15:36:27 +0600

    added login page js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-console.log("app.js connected");
+// User login
+const loginBtn = document.getElementById("login-btn");
+const loginIdInput = document.getElementById("login-id");
+const loginPassInput = document.getElementById("login-pass");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+        const loginId = loginIdInput.value.trim();
+        const loginPass = loginPassInput.value.trim();
+
+        // Verify username & password
+        if (loginId === "admin" && loginPass === "admin123") {
+            window.location.assign("/main.html");
+        } else {
+            alert("Incorrect Login Details!");
+        }
+    });
+}
